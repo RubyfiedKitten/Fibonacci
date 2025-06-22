aa83579bcffba9bd9b23cd35f812b70a7b63bfe6
RubyfiedKitten <rossouwhennie2@gmail.com>
2025-06-22 15:27:42 +0200

    1
    Updated with BigInts
    
    Can handle up to 1000th fibonacci now

--- fibonacci-app/Main.gd
+++ fibonacci-app/Main.gd
@@ -1,31 +1,29 @@
 extends Node2D
 
+@warning_ignore("shadowed_global_identifier")
+const Big = preload("res://Godot 4.1.x/Big.gd")
 @onready var line_edit: LineEdit = $LineEdit
 @onready var output_label: Label = $OutputLabel
 @onready var texture_button: TextureButton = $TextureButton
+
+var a : Big = Big.new(0)
+var b : Big = Big.new(1)
 
 func _ready() -> void:
 	line_edit.text_submitted.connect(_on_input_given)
 
 func _on_input_given(input: String):
 	var number : int = input.to_int()
-	if (number > 0 and number <= 5555):
-		output_label.text = calc_Fibonacci(number)
+	if number <= 1000:
+		output_label.text = calc_Fibonacci(number, a, b)
 	else:
-		output_label.text = "That's not a valid number, please try again :D"
+		output_label.text = "Too many loops, crashing lol"
 
-func calc_Fibonacci(n): # will take in a vaild number from the input panel, and return a string :D
-	var a : int = 0
-	var b : int = 1
-	var ans = 0
-	for i in range(n):
-		if n < 2:
-			ans = 1
-		else: 
-			ans = a + b
-			a = b
-			b = ans
-	return str(a)
+@warning_ignore("shadowed_variable")
+func calc_Fibonacci(n, a , b): # will take in a vaild number from the input panel, and return a string :D
+	if n < 2:
+		return a.toScientific()
+	return calc_Fibonacci(n-1, b, a.plus(b))
 
 func _on_texture_button_pressed() -> void:
 	output_label.text = "Thank you for petting Mr. Snuggles Worth 🥰"
